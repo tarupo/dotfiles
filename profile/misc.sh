6a07652dd80b39b5b4dcaba53318db11a76ebb99
tarupo <kendsheld@outlook.com>
2024-02-09 13:59:34 +0800

    update profile open func

--- profile/misc.sh
+++ profile/misc.sh
@@ -1,1 +1,9 @@
-alias open=/mnt/c/Windows/explorer.exe
+open() {
+    if [ -z $1 ];
+    then
+        /mnt/c/Windows/explorer.exe .
+        return
+    fi
+    p="$(readlink -f $1)"
+    /mnt/c/Windows/explorer.exe "$(wslpath -w $p)"
+}
