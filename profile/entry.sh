1f1fef44e5c93e18c174773c35d2c4f89790c35d
tarupo <kendsheld@outlook.com>
2026-01-12 11:09:10 +0800

    feat: update ENTRY_ROOT init

--- profile/entry.sh
+++ profile/entry.sh
@@ -1,4 +1,4 @@
-base_path=$(dirname $0)
+base_path=$DOT_PROFILE_ROOT
 
 source "$base_path/$(basename ./proxyrc.sh)"
 source "$base_path/$(basename ./misc.sh)"
--- profile/init.sh
+++ profile/init.sh
@@ -1,8 +1,14 @@
 # entry setup
+echo "" >> ~/.profile
+echo "# DOTFILES PROFILE" >> ~/.profile
 ENTRY_PATH=$(readlink -f ./entry.sh)
+DOT_PROFILE_ROOT=$(dirname $ENTRY_PATH)
+
+echo "export DOT_PROFILE_ROOT=$DOT_PROFILE_ROOT" >> ~/.profile
 echo "source $ENTRY_PATH" >> ~/.profile
 
 # proxy setup
 echo 'export PROXY_HOST="localhost"' >> ~/.profile # set proxy host 
 echo 'export MIX_PROXY_PORT=""' >> ~/.profile # set proxy port
-
+echo "# DOTFILES PROFILE END" >> ~/.profile
+echo "" >> ~/.profile
